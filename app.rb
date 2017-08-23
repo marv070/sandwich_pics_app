9b54be92dbb4aacb10cd0e836c1d1f1f51fa2363
Marvin Laucher <marv070@gmail.com>
2017-08-23 12:37:55 -0400

    update with deploy files

--- app.rb
+++ app.rb
@@ -1,34 +1,34 @@
 require "sinatra"
 require_relative "make_order.rb" 
+
 get '/' do 
 erb	:sandwich_parts
 end
+
 post '/sandwich_parts' do
 
 	toppings = params[:toppings].to_s
-	redirect'/order?toppings=' + toppings
+	toppings_with_join_method = params[:toppings].join(',')
+	p "what does toppings.to_s look like: #{toppings}"
+	p "what if instead i use toppings.join(',') #{toppings_with_join_method}"
+	 redirect'/order?toppings=' + toppings_with_join_method
 end
 get '/order' do
 
-	toppings = params[:toppings].split
+	toppings = params[:toppings].split(',')
 	erb :order, :locals=> {:toppings => toppings}
 end
 post '/order' do
-	toppings = params[:toppings].to_s
-	value = params[:value].to_s
-	redirect '/confimation?toppings=' + toppings + '&value=' + value
+	 inputs = params.values.join(',')
+	 p "#{inputs.class}what class is this"
+	
+	  redirect '/confimation?toppings=' + inputs
+	
 end
 get  '/confimation'do
 puts "MADE IT TO get confimation DO"
-	toppings = params[:toppings].split
+	toppings = params[:toppings].split(',')
 	puts "TOPPINGS + #{toppings}"
-	value = params[:value].split
-	erb :confimation, :locals => {:toppings => toppings, :value=>value}
-end
-post '/confimation'do
-puts "MADE IT TO post confimation DO"
-	toppings = params[:toppings].to_s
-	puts "TOPPINGS + #{toppings}"
-	value = params[:value].to_s
-	erb :confimation, :locals=>{:toppings=>toppings, :value=>value}
+	# value = params[:value].split
+	erb :confimation, :locals => {:toppings => toppings}
 end
